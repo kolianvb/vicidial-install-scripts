08714d2bf7bd13ef9780fc918a22294eacea0f6a
kolianvb <139160739+kolianvb@users.noreply.github.com>
2024-06-20 23:03:15 -0400

    Update vicidial-install-c7.sh

--- vicidial-install-c7.sh
+++ vicidial-install-c7.sh
@@ -160,7 +160,6 @@ yum install perl-DBI -y
 yum install perl-DBD-MySQL -y
 yum install perl-GD -y
 
-echo "Please Press ENTER for CPAN Install"
 
 yum install perl-CPAN -y
 yum install perl-YAML -y
@@ -168,58 +167,10 @@ yum install perl-libwww-perl -y
 yum install perl-DBI -y
 yum install perl-DBD-MySQL -y
 yum install perl-GD -y
-cd /usr/bin/
-curl -LOk http://xrl.us/cpanm
-chmod +x cpanm
-cpanm -f File::HomeDir
-cpanm -f File::Which
-cpanm CPAN::Meta::Requirements
-cpanm -f CPAN
-cpanm YAML
-cpanm MD5
-cpanm Digest::MD5
-cpanm Digest::SHA1
-cpanm readline --force
-cpanm Bundle::CPAN
-cpanm DBI
-cpanm -f DBD::mysql
-cpanm Net::Telnet
-cpanm Time::HiRes
-cpanm Net::Server
-cpanm Switch
-cpanm Mail::Sendmail
-cpanm Unicode::Map
-cpanm Jcode
-cpanm Spreadsheet::WriteExcel
-cpanm OLE::Storage_Lite
-cpanm Proc::ProcessTable
-cpanm IO::Scalar
-cpanm Spreadsheet::ParseExcel
-cpanm Curses
-cpanm Getopt::Long
-cpanm Net::Domain
-cpanm Term::ReadKey
-cpanm Term::ANSIColor
-cpanm Spreadsheet::XLSX
-cpanm Spreadsheet::Read
-cpanm LWP::UserAgent
-cpanm HTML::Entities
-cpanm HTML::Strip
-cpanm HTML::FormatText
-cpanm HTML::TreeBuilder
-cpanm Time::Local
-cpanm MIME::Decoder
-cpanm Mail::POP3Client
-cpanm Mail::IMAPClient
-cpanm Mail::Message
-cpanm IO::Socket::SSL
-cpanm MIME::Base64
-cpanm MIME::QuotedPrint
-cpanm Crypt::Eksblowfish::Bcrypt
-cpanm Crypt::RC4
-cpanm Text::CSV
-cpanm Text::CSV_XS
-
+yum install -y glibc-devel.i686 perl-CPAN perl-YAML perl-libwww-perl perl-DBI perl-DBD-MySQL perl-GD perl-Env perl-Term-ReadLine-Gnu perl-SelfLoader perl-open.noarch
+cd /usr/src/vicidial-install-scripts
+curl -fsSL https://raw.githubusercontent.com/skaji/cpm/main/cpm | perl - install -g App::cpm
+/usr/local/bin/cpm install -g
 
 #Install Asterisk Perl 
 cd /usr/src
